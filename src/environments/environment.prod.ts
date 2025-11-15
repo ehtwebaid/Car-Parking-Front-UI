ec1a4f9021a3591f88e18c60100f920d676358f1
ehtwebaid <ehtwebaid@gmail.com>
2025-11-15 12:36:20 +0530

    Change Logo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://demoehtwebaid.com/backend',
+  apiBaseUrl: 'https://parkinourspot.com/backend',
   Stripe_Publishable_Key:'pk_test_51S7hPfGgwiL8meciEU9M2kg0m13Z5NXF5atkuFyaOeOzvE9twHCIFKVDL3Q9wCEb6J3dzZyg7Vaj0BMGd1kmxgNz00oedpWZgt'
 
 };
